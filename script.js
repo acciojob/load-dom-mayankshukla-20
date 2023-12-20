1511018ba2f427e5923f3fb419684523e3ce30b0
Mayank Shukla <117716383+mayankshukla-20@users.noreply.github.com>
2023-12-20 14:39:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const body = document.querySelector("body");
+const para = document.createElement("p");
+p.innerText = "DOM load success";
+body.appendChild(para);
